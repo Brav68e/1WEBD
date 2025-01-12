9ccc42d1f6596eb5b4eee792f726fc01824da457
brav68e <thomas.dannequi@gmail.com>
2025-01-12 13:33:29 +0100

    Add 4 static films using API in index

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -39,3 +39,61 @@ function hidden_burger(){
 hidden_burger()         // Use it at start to handle mobile case
 window.addEventListener('resize', hidden_burger)
 
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Get and display all the trend films using axios
+
+
+// A function that load 4 predetermine films 
+async function load4Films(){
+
+    // Default films
+    let films = ['absolution', 'wicked', 'sonic', 'lift'];
+    let results = [];
+
+    // For each film, do a 'fast request' and use the id to do a second request (more information with id, e.g : plot)
+    for(let i=0; i<films.length; i++){
+        try{
+            let film = await axios.get(`https://www.omdbapi.com/?apikey=f69141f9&s=${films[i]}&y=2024`)
+            film = await axios.get(`https://www.omdbapi.com/?apikey=f69141f9&i=${film['data']['Search'][0]['imdbID']}`)
+            results.push(film['data'])
+        }
+        catch(e){
+            console.log(e)
+        }
+    }
+    
+    // console.log(results)
+    return results
+}
+
+
+async function display4Films(){
+
+    let films = await load4Films()
+
+    for(let i=0; i<films.length; i++){
+        let film = films[i]
+        let article = document.createElement("article")
+        article.innerHTML = `
+        <div>
+            <h2>${film['Title']}</h2>
+            <img src=${film['Poster']}>
+        </div>
+        <div>
+            <p>${film['Plot']}</p>
+            <a href="Assets/html/movie.html">En Savoir plus</a>
+        </div>
+        `
+        // console.log(article)
+        main.appendChild(article)
+    }
+}
+
+
+const main = document.querySelector('main');
+display4Films()
+
+
